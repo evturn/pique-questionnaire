d7f10749beb61538bdbd5eb6a3554dcf6d573bee
Evan Turner <evturn@gmail.com>
2015-04-03 05:04:57 -0400

    creating question/options objects that will become a collection of backbone models

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,38 @@ new surv.Home();
 new WOW(
     { offset: 120 }
 ).init();
+
+
+var questionnaire = [
+	{
+		question: '',
+		id: 1,
+		options: [
+
+		]
+	},
+	{
+		question: '',
+		id: 2,
+		options: [
+
+		]
+	},
+	{
+		question: '',
+		id: 3,
+		options: [
+
+		]
+	},
+	{
+		question: '',
+		id: 4,
+		options: [
+
+		]
+	}
+];
+
+
+
