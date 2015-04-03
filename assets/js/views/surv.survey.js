b09ac9c3abd5bbda85fd9aad5051bb1ae2ff8621
Evan Turner <evturn@gmail.com>
2015-04-03 06:17:38 -0400

    each model triggers next one on click

--- assets/js/views/surv.survey.js
+++ assets/js/views/surv.survey.js
@@ -11,16 +11,19 @@ surv.Survey = Backbone.View.extend({
 	},
   rotate: function(e) {
     e.preventDefault();
-    var id = $(e.currentTarget);
-    console.log(id);
-    var next = id += 1;
-    var question = this.collection.get(next);
-    this.renderQuestion(question);
+    var id = $(e.currentTarget).data('id');
+    if (id === 4) {
+    	alert('You\'re done. Thanks.')
+    	return false;
+    } else {
+	    var next = id += 1;
+	    var question = this.collection.get(next);
+	    this.renderQuestion(question);
+    }
   },
 	renderQuestion: function(model) {
 		var model = model || this.collection.get(1);
-		
-			this.$el.append(this.surveyTemplate(model.toJSON()));
-		
+		this.$el.append(this.surveyTemplate(model.toJSON()));
+		return this;
 	},
 });
